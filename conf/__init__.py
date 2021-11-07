9ae2419da53aad06f7938b290cc8f86362a37435
HIT-LiuChen <1487785442@qq.com>
2021-11-07 11:10:07 +0800

    add some notes

--- conf/__init__.py
+++ conf/__init__.py
@@ -4,10 +4,12 @@ author baiyu
 """
 import conf.global_settings as settings
 
+## coding tips: load config settings to Class dynamically
 class Settings:
     def __init__(self, settings):
 
         for attr in dir(settings):
+            ## build-in variable or build-in function is lowercase, so the settings in config file are uppercase which are coded by author.
             if attr.isupper():
                 setattr(self, attr, getattr(settings, attr))
 
